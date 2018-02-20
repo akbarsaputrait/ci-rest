971ecd95bace9769d140cc8423f9cc608d32677c
akbarsaputra <akbarsaputra45@hotmail.com>
2018-02-20 10:34:11 +0700

    fix!!!

--- application/controllers/Rest.php
+++ application/controllers/Rest.php
@@ -1,71 +1,78 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . '../vendor/autoload.php';
 require APPPATH . '/libraries/REST_Controller.php';
 use \Firebase\JWT\JWT;
 
-class Rest extends REST_Controller {
+class Rest extends REST_Controller
+{
     private $secretkey = 'ZrmjJpOLm9';
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         $this->load->library('form_validation');
         $this->load->database();
+        date_default_timezone_set('Asia/Jakarta');
     }
 
-    // method untuk melihat token pada user
-    public function generate_post(){
-        header('Content-Type: application/json'); 
+    public function login_post()
+    {
+        header('Content-Type: application/json');
         $this->load->model('loginmodel');
-        $username = $this->post('username',TRUE);
-        $pass = $this->post('password',TRUE);
+        $username = $this->post('username', true);
+        $pass = $this->post('password', true);
         $data = $this->loginmodel->is_valid($username);
 
-       /* echo json_encode(password_hash($dataadmin->password, PASSWORD_DEFAULT));exit;*/
-
         if ($data) {
-            if (password_verify($this->post('password'),password_hash($data->password, PASSWORD_DEFAULT))) {
+            if (password_verify($this->post('password'), password_hash($data->password, PASSWORD_DEFAULT))) {
                 $payload['id'] = $data->id;
                 $payload['username'] = $data->username;
                 $payload['email'] = $data->email;
-                $payload['pc'] = $data->pc;
-                $payload['date'] = date('Y-m-d H:i:s'); //waktu di buat
-                $output['token'] = JWT::encode($payload,$this->secretkey);
+                $payload['date'] = date('d-m-Y H:i:s');
+                $output['token'] = JWT::encode($payload, $this->secretkey);
                 // return $this->response($output,REST_Controller::HTTP_OK);
                 echo json_encode(array('data' => $payload, 'token' => $output, 'success' => true));
-                $this->db
-                ->set('token', $output['token'])
-                ->where('id', $data->id)
-                ->update('client');
+
+                // $this->db
+                //     ->set('token', $output['token'])
+                //     ->where('token', $data->token)
+                //     ->update('client');
+                // $this->db
+                //     ->set('token', $output['token'])
+                //     ->where('token', $data->token)
+                //     ->update('task');
+                // $this->db
+                //     ->insert('history_logs', array('username' => $payload['username'], 'pc' => $payload['pc'], 'date' => $payload['date'], 'status' => 'Login', 'token' => $output['token']));
             } else {
-                echo json_encode(array('error' => true));
+                echo json_encode(array('error' => true, 'message' => 'Token not generated'));
             }
-
         } else {
-            echo json_encode(array('error' => true));
+            echo json_encode(array('error' => true, 'message' => 'User not registered'));
         }
     }
 
-    // method untuk mengecek token setiap melakukan post, put, etc
-    public function cektoken(){
-        header('Content-Type: application/json'); 
-
-        $this->load->model('loginmodel');
-
-        $jwt = $this->input->get_request_header('Authorization');
-
-       if($decode = JWT::decode($jwt,$this->secretkey,array('HS256'))){
-
-            if ($this->loginmodel->is_valid_num($decode->username) > 0) {
-                return true;
+    public function check_post()
+    {
+        header('Content-Type: application/json');
+        $header = $this->input->get_request_header('Authorization', true);
+        if ($decode = JWT::decode($header, $this->secretkey, array('HS256'))) {
+            $data = $this->db->query('SELECT * FROM client WHERE id = "' . $decode->id . '" LIMIT 1 ');
+            if ($data) {
+                $client = array();
+                foreach ($data->result() as $row) {
+                    $client['id'] = $row->id;
+                    $client['username'] = $row->username;
+                    $client['email'] = $row->email;
+                    $client['pc'] = $row->pc;
+                }
+                // echo json_encode($client);
+                return $client;
             }
-
-        } else {
-            echo json_encode(array('status' => '0' ,'message' => 'Invalid Token',));
+            return false;
         }
+        return false;
     }
-
 }
-?>
